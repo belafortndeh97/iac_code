3b89fd47e537e514e26fa1a3b368e688a5c4003c
belafort <Belafortndeh97@gmail.com>
2024-11-08 22:21:52 -0500

    updated terraform files

--- terraform/terraform.tf
+++ terraform/terraform.tf
@@ -1,15 +1,15 @@
 terraform {
   required_providers {
     aws = {
-        source = "hashicorp/aws"
-        version = ">= 5.25.0"
+      source  = "hashicorp/aws"
+      version = ">= 5.25.0"
     }
   }
 
   backend "s3" {
-    bucket = "cloudcore007"
-    key    = "terraform.tfstate"
-    region = "us-east-1"
+    bucket = "terraform-state-file-aosnote"
+    key    = "terraform.tfstate.dev"
+    region = "us-east-2"
   }
 
   required_version = ">= 1.6.3"
